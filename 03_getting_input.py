5ff743d1a78c8308a3f47bcf2489e642daa6753a
lingj1206 <lingj1206@masseyhigh.school.nz>
2022-03-15 12:19:53 +1300

    Create 03_getting_input.py

---
+++ 03_getting_input.py
@@ -0,0 +1,9 @@
+username = input("What is your name? ")
+
+fav_num = int(input("What is your favorite number? "))
+
+double = fav_num * 2
+half = fav_num / 2
+squared = fav_num * fav_num
+
+print("Hi, {}, your favorite number is {}".format(username, fav_num))
