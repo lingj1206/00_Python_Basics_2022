9568fb6d7e9c862d1faf0e7d5f7e66f8918d9339
lingj1206 <lingj1206@masseyhigh.school.nz>
2022-03-17 09:24:02 +1300

    Update 03_getting_input.py

--- 03_getting_input.py
+++ 03_getting_input.py
@@ -1,3 +1,4 @@
+print()
 username = input("What is your name? ")
 
 fav_num = int(input("What is your favorite number? "))
@@ -5,5 +6,12 @@ fav_num = int(input("What is your favorite number? "))
 double = fav_num * 2
 half = fav_num / 2
 squared = fav_num * fav_num
+print()
 
-print("Hi, {}, your favorite number is {}".format(username, fav_num))
+print("Hi {}, your favorite number is {}".format(username, fav_num))
+
+print()
+print("double {} is {}".format(fav_num, double))
+print("half of {} is {}".format(fav_num, half))
+print("{} squared is {}".format(fav_num, squared))
+print()
